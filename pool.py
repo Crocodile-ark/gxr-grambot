80ebbc8778572457e69447ec1827aa7a95184a07
Crocodile-ark <wibudesu1709@gmail.com>
2025-06-25 16:20:19 +0900

    Update pool.py

--- pool.py
+++ pool.py
@@ -1,1 +1,17 @@
+# pool.py
 
+from config import POOL_PER_EVOL
+
+# Simulasi memori, bisa pakai file kalau mau persistent
+evol_pool_used = {name: 0 for name in POOL_PER_EVOL.keys()}
+
+def check_and_reduce_pool(evol_name, amount):
+    # Deteksi level dari nama
+    for i in range(1, 8):
+        if f"Evol {i}" in evol_name:
+            if evol_pool_used[i] + amount > POOL_PER_EVOL[i]:
+                return False
+            evol_pool_used[i] += amount
+            return True
+    return False
+  
